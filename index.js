62efea6a11c5f1cbe60e947577bbfd9b714e93ba
tsumitakaoru <k.tsumita01@gmail.com>
2022-08-20 17:09:21 -0400

    Add keypress eventlistner

--- index.js
+++ index.js
@@ -3,12 +3,57 @@ const buttons = document.querySelectorAll(".drum");
 for (let i = 0; i < buttons.length; i++) {
 
   buttons[i].addEventListener("click", function() {
-
     const buttonInnerHTML = this.innerHTML;
     console.log(buttonInnerHTML);
-
   });
 
 }
 
+document.addEventListener("keypress", (event) => {
+  makeSound(event.key);
+});
 
+
+const makeSound = (key) => {
+
+  switch (key) {
+    case "w":
+      const crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case "a":
+      const kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+    case "s":
+      const snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case "d":
+      const tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case "j":
+      const tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case "k":
+      const tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+
+    case "l":
+      const tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+
+    default: console.log(key);
+
+  }
+}
